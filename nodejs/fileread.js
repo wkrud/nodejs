f5b6bf58fa449b72d3d6ee35e9d7d8e20d5edb1b
wkrud <jae6140@naver.com>
2021-12-20 09:20:11 +0900

    Revert "nodejs study"
    This reverts commit cc99bd61e175984fc377de2bd3b214bfb1a67cf4.

--- nodejs/fileread.js
+++
@@ -1,8 +0,0 @@
-var fs = require('fs');
-// fs.readFile('sample.txt', 'utf8', function(err, data){
-//     console.log(data);
-// });
-// fs.readFile(경로, 인코딩, 콜백함수)
-fs.readFile('sample.txt', 'utf8', (err, data) => {
-    console.log(data);
-});
