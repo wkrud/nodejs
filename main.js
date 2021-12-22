b20a7a8eaaaf11c245273be648864c298f064080
wkrud <jae6140@naver.com>
2021-12-22 09:55:16 +0900

    함수

--- main.js
+++ main.js
@@ -1,19 +1,14 @@
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
+var http = require("http");
+var fs = require("fs");
+var url = require("url");
 
-var app = http.createServer(function(request,response){
-    var _url = request.url;
-    var queryData = url.parse(_url, true).query;
-    var pathname = url.parse(_url, true).pathname;
-    
-    
-    if(pathname === '/'){
-      if(queryData.id === undefined){
-        fs.readFile(`data/${queryData.id}`, 'utf8', (err, description) => {
-          var title = 'Welcome';
-          var description = 'Hello, Node.js';
-          var template = `
+var app = http.createServer(function (request, response) {
+  var _url = request.url;
+  var queryData = url.parse(_url, true).query;
+  var pathname = url.parse(_url, true).pathname;
+
+  function templateHTML(title, list, body) {
+    return `
       <!doctype html>
       <html>
       <head>
@@ -22,51 +17,56 @@ var app = http.createServer(function(request,response){
       </head>
       <body>
         <h1><a href="/">WEB</a></h1>
-        <ol>
-          <li><a href="/?id=HTML">HTML</a></li>
-          <li><a href="/?id=CSS">CSS</a></li>
-          <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ol>
-        <h2>${title}</h2>
-        <p>${description}</p>
+        ${list}        
+        ${body}
       </body>
       </html>
-          `;
-          response.writeHead(200);
-          response.end(template);
-        });
-      }else{
-        fs.readFile(`data/${queryData.id}`, 'utf8', (err, description) => {
-          var title = queryData.id;  
-          var template = `
-      <!doctype html>
-      <html>
-      <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-      </head>
-      <body>
-        <h1><a href="/">WEB</a></h1>
-        <ol>
-          <li><a href="/?id=HTML">HTML</a></li>
-          <li><a href="/?id=CSS">CSS</a></li>
-          <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ol>
-        <h2>${title}</h2>
-        <p>${description}</p>
-      </body>
-      </html>
-          `;
-          response.writeHead(200);
-          response.end(template);
-        });
-      }
-      
-    }else{
-      response.writeHead(404);
-      response.end('Not found');
+      `;
+  }
+
+  function templateList(filelist) {
+    var list = "<ul>";
+    var i = 0;
+    while (i < filelist.length) {
+      list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+      i++;
     }
+    list = list + "</ul>";
+    return list;
+  }
 
- 
+  if (pathname === "/") {
+    if (queryData.id === undefined) {
+      fs.readdir("./data", function (error, filelist) {
+        var title = "Welcome";
+        var description = "Hello, Node.js";
+        var list = templateList(filelist);
+        var template = templateHTML(
+          title,
+          list,
+          `<h2>${title}</h2>${description}`
+        );
+        response.writeHead(200);
+        response.end(template);
+      });
+    } else {
+      fs.readdir("./data", function (error, filelist) {
+        fs.readFile(`data/${queryData.id}`, "utf8", (err, description) => {
+          var list = templateList(filelist);
+          var title = queryData.id;
+          var template = templateHTML(
+            title,
+            list,
+            `<h2>${title}</h2>${description}`
+          );
+          response.writeHead(200);
+          response.end(template);
+        });
+      });
+    }
+  } else {
+    response.writeHead(404);
+    response.end("Not found");
+  }
 });
 app.listen(3000);
---
+++ syntax/function2.js
@@ -0,0 +1,12 @@
+// 반올림
+console.log(Math.round(1.6));
+console.log(Math.round(1.4));
+
+function sum(first, second){
+    return first + second;
+}
+const sume = (i, e) => {
+    console.log(i + e);
+};
+
+console.log(sum(2,4));
