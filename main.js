3080d6054c4ad10ab323b2868fe401984d3f1158
wkrud <jae6140@naver.com>
2021-12-23 11:45:44 +0900

    post send

--- main.js
+++ main.js
@@ -2,39 +2,40 @@ var http = require("http");
 var fs = require("fs");
 var url = require("url");
 
+function templateHTML(title, list, body) {
+  return `
+    <!doctype html>
+    <html>
+    <head>
+      <title>WEB1 - ${title}</title>
+      <meta charset="utf-8">
+    </head>
+    <body>
+      <h1><a href="/">WEB2</a></h1>
+      ${list}   
+      <a href="/create">create</a>     
+      ${body}
+    </body>
+    </html>
+    `;
+}
+
+function templateList(filelist) {
+  var list = "<ul>";
+  var i = 0;
+  while (i < filelist.length) {
+    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+    i++;
+  }
+  list = list + "</ul>";
+  return list;
+}
+
 var app = http.createServer(function (request, response) {
   var _url = request.url;
   var queryData = url.parse(_url, true).query;
   var pathname = url.parse(_url, true).pathname;
-
-  function templateHTML(title, list, body) {
-    return `
-      <!doctype html>
-      <html>
-      <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-      </head>
-      <body>
-        <h1><a href="/">WEB</a></h1>
-        ${list}        
-        ${body}
-      </body>
-      </html>
-      `;
-  }
-
-  function templateList(filelist) {
-    var list = "<ul>";
-    var i = 0;
-    while (i < filelist.length) {
-      list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-      i++;
-    }
-    list = list + "</ul>";
-    return list;
-  }
-
+    
   if (pathname === "/") {
     if (queryData.id === undefined) {
       fs.readdir("./data", function (error, filelist) {
@@ -64,7 +65,39 @@ var app = http.createServer(function (request, response) {
         });
       });
     }
+  } else if(pathname === '/create'){
+    fs.readdir("./data", function (error, filelist) {
+      var title = "WEB - create";
+      var list = templateList(filelist);
+      var template = templateHTML(title, list, `
+      <form action="http://localhost:3000/create_process"
+      method="post">
+        <p><input type="text" name="title" placeholder="title"></p>
+        <p>
+          <textarea name="description" placeholder="description"></textarea>
+        </p>
+        <p>
+          <input type="submit">
+        </p>
+      </form>
+      `
+      );
+      response.writeHead(200);
+      response.end(template);
+    });
+  } else if(pathname === '/create_process'){
+    response.writeHead(200);
+    response.end('sucuess');
   } else {
+    var body = '';
+    request.on('data', function(data){
+      body = body + data;
+    });
+    request.on('end', function(){
+      var title = new URLSearchParams(body).get('title');
+      var description = new URLSearchParams(body).get('description');
+      console.log(title + description);
+    });
     response.writeHead(404);
     response.end("Not found");
   }
